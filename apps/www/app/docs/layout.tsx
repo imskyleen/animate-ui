d08aa438f2115f9800f3282dfb4992862f397cbe
Elliot Sutton <elsk8@outlook.fr>
2025-09-16 18:22:40 +0200

    feat: add dialog components

--- apps/www/app/docs/layout.tsx
+++ apps/www/app/docs/layout.tsx
@@ -12,50 +12,6 @@ const DOCS_LAYOUT_PROPS: DocsLayoutProps = {
   tree: source.pageTree,
   sidebar: {
     tabs: SIDEBAR_TABS,
-    // tabs: [
-    //   {
-    //     title: 'Components',
-    //     description: 'Animated Components',
-    //     icon: (
-    //       <div className="[&_svg]:size-full rounded-lg size-full text-muted-foreground max-md:bg-(--tab-color)/10 max-md:border max-md:p-1.5">
-    //         <Component />
-    //       </div>
-    //     ),
-    //     url: '/docs/components',
-    //   },
-    //   {
-    //     title: 'Primitives',
-    //     description: 'Animated Primitives',
-    //     icon: (
-    //       <div className="[&_svg]:size-full rounded-lg size-full text-muted-foreground max-md:bg-(--tab-color)/10 max-md:border max-md:p-1.5">
-    //         <Cuboid />
-    //       </div>
-    //     ),
-    //     url: '/docs/primitives',
-    //   },
-    //   {
-    //     title: (
-    //       <span>
-    //         Icons{' '}
-    //         <span
-    //           className={cn(
-    //             dancing.className,
-    //             'text-sm ml-2 text-blue-600 dark:text-blue-400',
-    //           )}
-    //         >
-    //           beta
-    //         </span>
-    //       </span>
-    //     ),
-    //     description: 'Animated Icons',
-    //     icon: (
-    //       <div className="[&_svg]:size-full rounded-lg size-full text-muted-foreground max-md:bg-(--tab-color)/10 max-md:border max-md:p-1.5">
-    //         <LucideIcons />
-    //       </div>
-    //     ),
-    //     url: '/docs/icons',
-    //   },
-    // ],
   },
 
   githubUrl: 'https://github.com/imskyleen/animate-ui',
