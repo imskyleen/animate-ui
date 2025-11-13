0e5a1a928b552210faa996466ee4bb66f0aa2cb6
Elliot Sutton <elsk8@outlook.fr>
2025-11-13 10:21:21 +0100

    chore: up version

--- apps/www/registry/demo/components/community/flip-card/index.tsx
+++ apps/www/registry/demo/components/community/flip-card/index.tsx
@@ -2,20 +2,20 @@
 
 import { FlipCard } from '@/registry/components/community/flip-card';
 
-export const FlipCardDemo = () => {
-  const data = {
-    name: 'Animate UI',
-    username: 'animate_ui',
-    image:
-      'https://pbs.twimg.com/profile_images/1950218390741618688/72447Y7e_400x400.jpg',
-    bio: 'A fully animated, open-source component distribution built with React, TypeScript, Tailwind CSS, and Motion.',
-    stats: { following: 200, followers: 2900, posts: 120 },
-    socialLinks: {
-      linkedin: 'https://linkedin.com',
-      github: 'https://github.com',
-      twitter: 'https://twitter.com',
-    },
-  };
+const data = {
+  name: 'Animate UI',
+  username: 'animate_ui',
+  image:
+    'https://pbs.twimg.com/profile_images/1950218390741618688/72447Y7e_400x400.jpg',
+  bio: 'A fully animated, open-source component distribution built with React, TypeScript, Tailwind CSS, and Motion.',
+  stats: { following: 200, followers: 2900, posts: 120 },
+  socialLinks: {
+    linkedin: 'https://linkedin.com',
+    github: 'https://github.com',
+    twitter: 'https://twitter.com',
+  },
+};
 
+export const FlipCardDemo = () => {
   return <FlipCard data={data} />;
 };
